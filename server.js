f9cc48743a34206c93669dfda4de99f7a20dd670
AfrazKhan <khan.afraz4183@yahoo.com>
2019-05-25 20:39:23 +0500

    Update server.js

--- server.js
+++ server.js
@@ -13,16 +13,34 @@ var port  = 3000
 
 mongodb.connect(db.url,{'useNewUrlParser':true}, (err, database) => {
     if (err) return console.log(err)
+    else console.log('MONgo is up')
     
     var DB = database.db('Afraz_Notable')
     var collection = DB.collection('notes')
     
     app.get('/',(req,rep)=>{
-        console.log(DB.collection('notes').find())
-        rep.render('note')
+      collection.find({}).toArray(function(err,result){
+        if(err) console.log('error')
+        else console.log(result); rep.render('note')
+      })
     })
+    // app.get('/',(req,rep)=>{
+    //   var note={'text':'asdasd','body':'sdfgh'}
+    //   DB.collection('notes').insert(note, (err, result) => {
+    //     if (err) { 
+    //       //rep.send({ 'error': 'An error has occurred' }); 
+    //       console.log('error')
+    //       rep.render('note')
+    //     } else {
+    //       console.log('gone')
+    //       rep.render('note')
+    //     }
+    //   });
+        
+    
 
-                   
+    
+             
   })
 
   app.listen(port, () => {
