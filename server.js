3a5308e6774bc6f013f851e05e615371e6397177
AfrazKhan <khan.afraz4183@yahoo.com>
2019-05-25 19:57:34 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
-const mongodb = require('mongodb');
+const mongodb = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const db             = require('./config/db');
+
 const app = express();
 
 app.set('view engine','ejs');
 app.use('/public/assets',express.static('public/assets'));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+let database;
+
+mongodb.connect(db.url, (err, database) => {
+    if (err) return console.log(err)
+    
+    app.listen(port, () => {
+      console.log('We are live on ' + port);
+    });               
+  })
+
 
 app.get('/',(req,rep)=>{
     rep.render('note')
