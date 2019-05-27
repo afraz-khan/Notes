3811c27c06f1e71e73b928872bb4de8fa7f6503e
AfrazKhan <khan.afraz4183@yahoo.com>
2019-05-27 17:24:57 +0500

    event binding completed

--- public/assets/main.js
+++ public/assets/main.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
     
     var add_btn = $('#plus');
     var add_notification = $('#add_notf');
-    
+    var update_btns = $('#notelist li #update_btn');
+    var delete_btn = $('#notelist li #delete_btn');
+
     // insert notification
     add_btn.click(function(e){
         e.preventDefault();
@@ -22,15 +24,100 @@ $(document).ready(function(){
                     setTimeout(function(){
                         add_notification.hide('slow',function(){
                             $('#add_text').val(" ")
-                             var item = '<li id='+id+  " class='list-group-item'><input type='text' class='form-control' value='"+note2+"' /><a href='#'' style='float:right;color:green' >Update</a> <span style='float:right' >|</span> <a style='float:right;color: firebrick;margin-left: 30px' href='#' >Delete</a></li>"
+                            
+                            var notecount = parseInt( $("#notecount").text());
+
+                            // var update_btn2 = document.createElement('a');
+                            // update_btn2.setAttribute('id','update_btn')
+                            // update_btn2.setAttribute('href','#')
+                            // update_btn2.style.color="green";
+                            // update_btn2.style.cssFloat='right';
+
+                            // var delete_btn2 = document.createElement('a');
+                            // update_btn2.setAttribute('id','delete_btn')
+                            // update_btn2.setAttribute('href','#')
+                            // update_btn2.style.color="firebrick";
+                            // update_btn2.style.cssFloat='right';
+                            // console.log(update_btn2)
+                            var update_btn1 = "<a id='update_btn' href='#' style='float:right;color:green' >Update</a>"
+                            var delete_btn1 = "<a id='delete_btn' style='float:right;color: firebrick;margin-left: 30px' href='#' >Delete</a>" 
+                            var item = "<li id="+id+" class='list-group-item'> <span style='font-size:13px;font-weight: bold' >"+(++notecount).toString()+
+                            "-</span> <input type='text' class='form-control' value="+note2+"/>"+
+                            "<a href='#' id='update_btn' style='float:right;color:green' >Update</a> <span style='float:right' >|</span>"+
+                             "<a id='delete_btn' style='float:right;color: firebrick;margin-left: 30px' href='#' >Delete</a></li>"
                       
                             // add new item to list
-                             $('#notelist').prepend(item)
-                             var notecount = parseInt( $("#notecount").text());
-                             $("#notecount").text(notecount=notecount+1)
+                             $('#notelist').append(item)
+                             $("#notecount").text(notecount)
+
+                             // update a note
+
+                             
+    update_btns = $('#notelist li #update_btn');
+    delete_btn = $('#notelist li #delete_btn');
+
+
+    $.each(update_btns,function(i,val){
+            
+        val.onclick = function(){
+            var li = val.parentElement;
+            var input = val.previousElementSibling.value;
+            var note = {id: li.getAttribute('id'),text:input};
+            $.ajax({
+                type:'put',
+                url:'/updatenote',
+                data:note,
+                success:function(Data){
+                    $('#success_alert').show('slow',function(){
+                    setTimeout(function(){
+                        $('#success_alert').hide()
+                    },3000)
+                    })
+                }
+            })
+        }
+    })
+
+
+    // delete a note
+
+    $.each(delete_btn,function(i,val){
+        val.onclick = function(){
+            var li = val.parentElement;
+            var note = {id:li.getAttribute('id')}
+
+            $.ajax({
+                type:'delete',
+                url:'/deletenote',
+                data:note,
+                success:function(Data){
+                    
+                    var ul = li.parentElement;
+                    var li_nxt = li.nextElementSibling;
+                    while(li_nxt){
+                        var children = li_nxt.children;
+                        var str = children[0].innerText;
+                        var num = parseInt(str.split('-')[0])
+                        num = num -1;
+                        children[0].innerText = (num).toString()+"-"; 
+                        li_nxt = li_nxt.nextElementSibling
+                    }
+                    ul.removeChild(li)
+                    var notecount = parseInt( $("#notecount").text());
+                    $("#notecount").text(notecount=notecount-1)
+                    $('#del_success_alert').show('slow',function(){
+                        setTimeout(function(){
+                            $('#del_success_alert').hide()
+                        },3000)
+                        })
+
+                }
+            })
+        }
+    })
                              
                             })
-                    },2000)
+                    },1000)
                 
                     
                 })
@@ -97,8 +184,6 @@ $(document).ready(function(){
 
     // update a note
 
-    var update_btns = $('#notelist li #update_btn');
-
     $.each(update_btns,function(i,val){
             
         val.onclick = function(){
@@ -123,8 +208,6 @@ $(document).ready(function(){
 
     // delete a note
 
-    var delete_btn = $('#notelist li #delete_btn');
-
     $.each(delete_btn,function(i,val){
         val.onclick = function(){
             var li = val.parentElement;
@@ -137,7 +220,24 @@ $(document).ready(function(){
                 success:function(Data){
                     
                     var ul = li.parentElement;
+                    var li_nxt = li.nextElementSibling;
+                    while(li_nxt){
+                        var children = li_nxt.children;
+                        var str = children[0].innerText;
+                        var num = parseInt(str.split('-')[0])
+                        num = num -1;
+                        children[0].innerText = (num).toString()+"-"; 
+                        li_nxt = li_nxt.nextElementSibling
+                    }
                     ul.removeChild(li)
+                    var notecount = parseInt( $("#notecount").text());
+                    $("#notecount").text(notecount=notecount-1)
+                    $('#del_success_alert').show('slow',function(){
+                        setTimeout(function(){
+                            $('#del_success_alert').hide()
+                        },3000)
+                        })
+
                 }
             })
         }
